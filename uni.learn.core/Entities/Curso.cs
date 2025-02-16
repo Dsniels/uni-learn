026e0c38ae5fcdad9e50d8243cbc08da414967b1
Daniel Salazar <dsnile.salazr@gmail.com>
2025-02-16 16:18:03 -0600

    Make Temas property nullable in Curso entity

--- uni.learn.core/Entities/Curso.cs
+++ uni.learn.core/Entities/Curso.cs
@@ -9,7 +9,7 @@ public class Curso : Base
     public string Titulo { get; set; }
     public string AuthorId { get; set; }
     public string Descripcion { get; set; }
-    public List<Temas> Temas { get; set; }
+    public List<Temas>? Temas { get; set; }
     public string Video { get; set; }
     public int Vistas { get; set; }
     public bool Aprobado { get; set; }
